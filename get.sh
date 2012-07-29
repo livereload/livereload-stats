b3d2c7d4bdc5fb64acecb56ef8ac27a1806215c5
Andrey Tarantsov <andrey@tarantsov.com>
2012-07-29 22:56:26 +0200

    Add get.sh to download Apache logs from NFSN

---
+++ get.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+mkdir -p data/apache-bz2
+mkdir -p data/apache
+echo RSYNC
+rsync -azv 'andreyvit_livereload@ssh.phx.nearlyfreespeech.net:/home/logs/access_log.*.bz2' data/apache-bz2/
+cd data/apache
+for i in ../apache-bz2/*.bz2; do
+  dst="$(basename "$i" .bz2)"
+
+  if /usr/bin/test "$i" -nt "$dst"; then
+    echo BUNZIP $(basename "$i")
+    bunzip2 -c $i >"$dst"
+  fi
+done
+for i in access_log.*[abcdef]; do
+  marker="$i.DONEZ"
+  if /usr/bin/test "$i" -nt "$marker"; then
+    echo "CONCAT $i"
+    cat $i >>${i%?}
+    touch $marker
+  fi
+done
