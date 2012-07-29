76e8ceaa88638833556ed5ac3285dd7a158616bf
Andrey Tarantsov <andrey@tarantsov.com>
2012-07-29 23:29:12 +0200

    Only run yearly data processing steps if -y has been passed

--- run.sh
+++ run.sh
@@ -1,13 +1,18 @@
 #! /bin/bash
+
+yearly=false
+if test "$1" = "-y"; then
+  yearly=true
+  echo "Will run yearly statistics."
+  shift
+fi
+
 node bin/process.js apache-to-raw apache raw "$@"
 
 node bin/process.js rawtodaily raw day-events "$@"
-
 node bin/process.js reduce day-events month-events "$@"
-node bin/process.js reduce day-events year-events "$@"
 
 node bin/process.js reducetemp month-events month-events-cum "$@"
-node bin/process.js reducetemp year-events year-events-cum "$@"
 
 node bin/process.js userinfo month-events-cum month-users "$@"
 
@@ -16,3 +21,8 @@ node bin/process.js usertemp month-users month-users-temp "$@"
 node bin/process.js segmentation month-users-temp month-segments "$@"
 
 node bin/report.js
+
+if $yearly; then
+  node bin/process.js reduce day-events year-events "$@"
+  node bin/process.js reducetemp year-events year-events-cum "$@"
+fi
