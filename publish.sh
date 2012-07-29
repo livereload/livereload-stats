ff7515d0a214837a5a8c14c409901f9024e26575
Andrey Tarantsov <andrey@tarantsov.com>
2012-07-29 23:49:27 +0200

    Fix publish.sh to actually publish

--- publish.sh
+++ publish.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "RSYNC data/html"
-rsync -vz data/html/ andreyvit_livereload@ssh.phx.nearlyfreespeech.net:/home/public/stats/
+rsync -vrz data/html/ andreyvit_livereload@ssh.phx.nearlyfreespeech.net:/home/public/stats/
 echo "http://livereload.com/stats/"
