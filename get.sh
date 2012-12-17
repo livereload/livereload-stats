f972707b01183163994c3aa72b974e4505691968
Andrey Tarantsov <andrey@tarantsov.com>
2012-12-17 08:50:39 +0700

    Fix get.sh to work on 10.8

--- get.sh
+++ get.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+newer_than() {
+  ! /bin/test -f "$2" || /bin/test "$1" -nt "$2"
+}
+
 mkdir -p data/apache-bz2
 mkdir -p data/apache
 echo "RSYNC data/apache-bz2"
@@ -7,14 +12,14 @@ cd data/apache
 for i in ../apache-bz2/*.bz2; do
   dst="$(basename "$i" .bz2)"
 
-  if /usr/bin/test "$i" -nt "$dst"; then
+  if newer_than "$i" "$dst"; then
     echo BUNZIP $(basename "$i")
     bunzip2 -c $i >"$dst"
   fi
 done
 for i in access_log.*[abcdef]; do
   marker="$i.DONEZ"
-  if /usr/bin/test "$i" -nt "$marker"; then
+  if newer_than "$i" "$marker"; then
     echo "CONCAT $i"
     cat $i >>${i%?}
     touch $marker
